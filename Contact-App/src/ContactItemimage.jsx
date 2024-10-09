dbe4ce204cb58852aae2677c7623f185074e69a2
4rip22 <kkruipputra816@gmail.com>
2024-10-10 02:02:04 +0700

    update ContactItemimage

---
+++ Contact-App/src/ContactItemimage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ContactItemimage({ imageUrl }) {
+    return (
+        <div className="contact-item__image">
+            <img src={imageUrl} alt="contact avatar" />
+        </div>
+    );
+}
+export default ContactItemimage;
