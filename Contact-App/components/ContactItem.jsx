7b0306704a3bcca952a8de5c9ad256890fbebe67
4rip22 <kkruipputra816@gmail.com>
2024-10-17 23:23:52 +0700

    update ContactItem

--- Contact-App/components/ContactItem.jsx
+++ Contact-App/components/ContactItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactItemBody from '../components/ContactItemBody';
 import ContactItemimage from '../components/ContactItemimage';
+import DeleteButton from './DeleteButton';
  
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag,onDelete }) {
  return (
    <div className="contact-item">
     <ContactItemimage imageUrl={imageUrl}/>
      <ContactItemBody name={name} tag={tag} />
+     <DeleteButton id={id} onDelete={onDelete}/>
    </div>
  );
 }
